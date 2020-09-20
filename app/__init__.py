e44e4bae11cc5d489cb81e8c882b6094f9f644f7
kiira254 <nkamotho57@gmail.com>
2020-09-20 20:51:48 +0300

    add error to the init

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from .config import DevConfig
 from flask_bootstrap import Bootstrap
+from app import views
+from app import error
 
 # Initializing application
 app = Flask(__name__)
