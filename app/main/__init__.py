eef7f34f8465f4a5375cc89ded02a5afc10d54f3
kiira254 <nkamotho57@gmail.com>
2020-09-20 21:23:05 +0300

    add blueprint

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,17 +1,3 @@
-from flask import Flask
-from .config import DevConfig
-from flask_bootstrap import Bootstrap 
-from app import views
-from app import error
-
-# Initializing application
-app = Flask(__name__)
-
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile("config.py")
-
-# Initializing Flask Extensions
-bootstrap = Bootstrap(app)
-
-from app import views
+from flask import Blueprint
+main = Blueprint('main',__name__)
+from . import views,errors
