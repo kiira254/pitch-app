0d3305b4281e33b8aabf5917e5f6b81f9a32fc50
kiira254 <nkamotho57@gmail.com>
2020-09-20 18:08:39 +0300

    add title into the html

--- app/views.py
+++ app/views.py
@@ -8,8 +8,8 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    message = 'Hello World'
-    return render_template('index.html',message = message)
+    title = 'Home-Pitches'
+    return render_template('index.html', title = title)
 
 @app.route('/pitch/<int:pitch_id>')
 def pitch(pitch_id):
