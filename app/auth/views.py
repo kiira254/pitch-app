08f8ddda799cfe5000211a55d46548c420d423e6
kiira254 <nkamotho57@gmail.com>
2020-09-21 17:23:55 +0300

    add email welcome message

--- app/auth/views.py
+++ app/auth/views.py
@@ -5,6 +5,7 @@ from ..models import User
 from .forms import LoginForm,RegistrationForm
 from .forms import RegistrationForm
 from .. import db
+from ..email import mail_message
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
@@ -35,7 +36,7 @@ def register():
         db.session.add(user)
         db.session.commit()
 
-        mail_message("Welcome to the Pitch App","email/welcome_user",user.email,user=user)
+        mail_message("Welcome to watchlist","email/welcome_user",user.email,user=user)
 
         return redirect(url_for('auth.login'))
         title = "New Account"
