b85d7f1729bbcafb950924f30cce883403793675
kiira254 <nkamotho57@gmail.com>
2020-09-21 05:18:44 +0300

    add changes

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,12 @@
-from flask import render_template
-from app import app
-from .models import review
+from flask import render_template,request,redirect,url_for
+from . import main
 from .forms import ReviewForm
+from ..models import Review
 
-Review = review.Review
+# Review = review.Review
 
 # Views
-@app.route('/')
+@main.route('/')
 def index():
 
     '''
@@ -15,30 +15,32 @@ def index():
     title = 'Pitches-Home page'
     return render_template('index.html', title = title)
 
-@app.route('/pitch/<int:pitch_id>')
+@main.route('/pitch/<int:pitch_id>')
 def pitch(pitch_id):
 
     '''
     View pitch page function that returns the pitch details page and its data
     '''
-    pitch = get_pitch(id)
-    title = f'{pitch.title}'
-    reviews = Review.get_reviews(pitch.id)
+    title = 'Pitches-Home page'
+    return render_template('pitch.html', title = title)
+    # pitch = get_pitch(id)
+    # title = f'{pitch.title}'
+    # reviews = Review.get_reviews(pitch.id)
 
-    return render_template('pitch.html',title = title,pitch = pitch,reviews = reviews)
+    # return render_template('pitch.html')
 
 
-@app.route('/pitch/review/new/<int:id>', methods = ['GET','POST'])
-def new_review(id):
-    form = ReviewForm()
-    pitch = get_pitch(id)
+# @main.route('/pitch/review/new/<int:id>', methods = ['GET','POST'])
+# def new_review(id):
+#     form = ReviewForm()
+#     # pitch = get_pitch(id)
 
-    if form.validate_on_submit():
-        title = form.title.data
-        review = form.review.data
-        new_review = Review(pitch.id,title,review)
-        new_review.save_review()
-        return redirect(url_for('pitch',id = pitch.id ))
+#     if form.validate_on_submit():
+#         title = form.title.data
+#         review = form.review.data
+#         new_review = Review(pitch.id,title,review)
+#         new_review.save_review()
+#         return redirect(url_for('pitch',id = pitch.id ))
 
-    title = f'{pitch.title} review'
-    return render_template('new_review.html',title = title, review_form=form, pitch=pitch)
+#     title = f'{pitch.title} review'
+#     return render_template('new_review.html',title = title, review_form=form, pitch=pitch)
